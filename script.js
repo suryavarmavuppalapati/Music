5a73fd83d21129e3613d0a94d8e8d66489262769
suryavarmavuppalapati <94167397+suryavarmavuppalapati@users.noreply.github.com>
2024-07-08 08:52:25 +0000

    change in js

--- script.js
+++ script.js
@@ -1,112 +1,105 @@
 const songs = [
-    {
-      name: "Kudmayi",
-      link: "https://paglasongs.com/files/download/id/14933",
-      artists: "Shahid Mallya",
-      image: "https://raw.githubusercontent.com/developergtm24/music-web/main/image%20musuic.jpg"
-    },
-    {
-      name: "Tum Se",
-      link: "https://pagalsongs.com.in/siteuploads/files/sfd3/1494/Tum%20Se-(PagalSongs.Com.IN).mp3",
-      artists: "Sachin-Jigar",
-      image: "https://raw.githubusercontent.com/developergtm24/music-web/main/image%20musuic.jpg"
-    },
-  ];
-  
-  var progress = document.querySelector("#progress");
-  var song = document.querySelector("#song");
-  var playBtn = document.querySelector("#play i");
-  var index = 0;
-  var img = document.querySelector(".img img");
-  
-  var title = document.querySelector("#title");
-  var thumb = document.querySelector("#thumb");
-  var artist = document.querySelector("#musician");
-  
-  var start = document.querySelector("#start");
-  var end = document.querySelector("#end");
-  
-  function loadSong(songIndex) {
-    song.src = songs[songIndex].link;
-    title.innerHTML = songs[songIndex].name;
-    artist.innerHTML = songs[songIndex].artists;
-    thumb.src = songs[songIndex].image;
-  
-    song.onloadedmetadata = function () {
-      progress.max = song.duration;
-      updateProgress();
-    };
-  }
-  
-  function updateProgress() {
-    setInterval(() => {
-      progress.value = song.currentTime;
-  
-      var min = Math.floor(song.duration / 60);
-      var sec = Math.floor(song.duration % 60);
-  
-      var curMin = Math.floor(song.currentTime / 60);
-      var curSec = Math.floor(song.currentTime % 60);
-  
-      if (sec < 10) {
-        sec = "0" + sec;
-      }
-      if (curSec < 10) {
-        curSec = "0" + curSec;
-      }
-      if (min < 10) {
-        min = "0" + min;
-      }
-      if (curMin < 10) {
-        curMin = "0" + curMin;
-      }
-  
-      start.innerHTML = `${curMin}:${curSec}`;
-      end.innerHTML = `${min}:${sec}`;
-  
-      if (song.currentTime == song.duration) {
-        nextPlay();
-      }
-    }, 1000);
-  }
-  
-  progress.onchange = function () {
-    song.currentTime = progress.value;
-    song.play();
-    playBtn.classList.remove("bx-play");
-    playBtn.classList.add("bx-pause");
-    img.classList.add("play");
+  {
+    name: "Kudmayi",
+    link: "https://paglasongs.com/files/download/id/14933",
+    artists: "Shahid Mallya",
+    image:
+      "https://raw.githubusercontent.com/developergtm24/music-web/main/image%20musuic.jpg",
+  },
+  {
+    name: "Tum Se",
+    link: "https://pagalsongs.com.in/siteuploads/files/sfd3/1494/Tum%20Se-(PagalSongs.Com.IN).mp3",
+    artists: "Sachin-Jigar",
+    image:
+      "https://raw.githubusercontent.com/developergtm24/music-web/main/image%20musuic.jpg",
+  },
+  {
+    name: "Sooseki",
+    link: "https://gaana.com/song/sooseki-from-pushpa-2-the-rule",
+    artists: "Sachin-Jigar",
+    image: "",
+  },
+];
+
+var progress = document.querySelector("#progress");
+var song = document.querySelector("#song");
+var playBtn = document.querySelector("#play i");
+var index = 0;
+var img = document.querySelector(".img img");
+
+var title = document.querySelector("#title");
+var artist = document.querySelector("#musician");
+
+var start = document.querySelector("#start");
+var end = document.querySelector("#end");
+
+function loadSong(songIndex) {
+  console.log("Loading song:", songs[songIndex]); // Debugging
+  song.src = songs[songIndex].link;
+  title.innerHTML = songs[songIndex].name;
+  artist.innerHTML = songs[songIndex].artists;
+  img.src = songs[songIndex].image;
+
+  song.onloadedmetadata = function () {
+    progress.max = song.duration;
+    end.innerHTML = formatTime(song.duration);
+    console.log("Song metadata loaded:", song.duration); // Debugging
   };
-  
-  function nextPlay() {
-    index = (index + 1) % songs.length;
-    loadSong(index);
-    song.play();
-  }
-  
-  function prevPlay() {
-    index = (index - 1 + songs.length) % songs.length;
-    loadSong(index);
-    song.play();
-  }
-  
-  playBtn.parentElement.addEventListener("click", function () {
-    if (song.paused) {
-      song.play();
-      playBtn.classList.remove("bx-play");
-      playBtn.classList.add("bx-pause");
-      img.classList.add("play");
-    } else {
-      song.pause();
-      playBtn.classList.remove("bx-pause");
-      playBtn.classList.add("bx-play");
-      img.classList.remove("play");
-    }
-  });
-  
-  document.querySelector("#next").addEventListener("click", nextPlay);
-  document.querySelector("#prev").addEventListener("click", prevPlay);
-  
-  // Initialize the first song
+
+  song.ontimeupdate = function () {
+    progress.value = song.currentTime;
+    start.innerHTML = formatTime(song.currentTime);
+  };
+
+  console.log("Song source set to:", song.src); // Debugging
+  console.log("Title set to:", title.innerHTML); // Debugging
+  console.log("Artist set to:", artist.innerHTML); // Debugging
+  console.log("Image source set to:", img.src); // Debugging
+}
+
+function formatTime(seconds) {
+  const min = Math.floor(seconds / 60);
+  const sec = Math.floor(seconds % 60);
+  return `${min < 10 ? "0" : ""}${min}:${sec < 10 ? "0" : ""}${sec}`;
+}
+
+progress.onchange = function () {
+  song.currentTime = progress.value;
+  song.play();
+  playBtn.classList.replace("bx-play", "bx-pause");
+  img.classList.add("play");
+};
+
+function nextPlay() {
+  index = (index + 1) % songs.length;
   loadSong(index);
-  
+  song.play();
+  playBtn.classList.replace("bx-play", "bx-pause");
+  img.classList.add("play");
+}
+
+function prevPlay() {
+  index = (index - 1 + songs.length) % songs.length;
+  loadSong(index);
+  song.play();
+  playBtn.classList.replace("bx-play", "bx-pause");
+  img.classList.add("play");
+}
+
+playBtn.parentElement.addEventListener("click", function () {
+  if (song.paused) {
+    song.play();
+    playBtn.classList.replace("bx-play", "bx-pause");
+    img.classList.add("play");
+  } else {
+    song.pause();
+    playBtn.classList.replace("bx-pause", "bx-play");
+    img.classList.remove("play");
+  }
+});
+
+document.querySelector("#next").addEventListener("click", nextPlay);
+document.querySelector("#prev").addEventListener("click", prevPlay);
+
+// Load the initial song
+loadSong(index);
